798015400fd8bfbd8249c9e9857c0b92a0a86526
hill.gao <hill.gao@yuansuan.com>
2023-12-04 16:06:52 +0800

    在64位电脑上，int和uint占8字节，rune占4字节，bool占1字节 int= 8 uint= 8 rune= 4 bool= 1

--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	fmt.Print("hello world")
+	var i int  // 8字节
+	var u uint // 8字节
+	var r rune // 4字节
+	var b bool // 1字节
+	fmt.Println("int=", unsafe.Sizeof(i))
+	fmt.Println("uint=", unsafe.Sizeof(u))
+	fmt.Println("rune=", unsafe.Sizeof(r))
+	fmt.Println("bool=", unsafe.Sizeof(b))
 }
