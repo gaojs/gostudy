578f8167a3c3e1db2747d7d9109797819b576b9f
hill.gao <hill.gao@yuansuan.com>
2023-12-04 17:19:21 +0800

    按位取反（^），也是按位异或（^）

--- src/main.go
+++ src/main.go
@@ -6,12 +6,17 @@ import (
 )
 
 func main() {
-	var i int  // 8字节
-	var u uint // 8字节
-	var r rune // 4字节
-	var b bool // 1字节
-	fmt.Println("int=", unsafe.Sizeof(i))
-	fmt.Println("uint=", unsafe.Sizeof(u))
-	fmt.Println("rune=", unsafe.Sizeof(r))
-	fmt.Println("bool=", unsafe.Sizeof(b))
+	var f32 float32     // 4字节
+	var f64 float64     // 8字节
+	var c64 complex64   // 8字节
+	var c128 complex128 // 16字节
+	fmt.Println("float32=", unsafe.Sizeof(f32))
+	fmt.Println("float64=", unsafe.Sizeof(f64))
+	fmt.Println("complex64=", unsafe.Sizeof(c64))
+	fmt.Println("complex128=", unsafe.Sizeof(c128))
+	var i uint8 = 3
+	var j = ^i       // 按位取反：252
+	var k = 0xFF ^ i // 按位异或:252
+	println("j=", j)
+	println("k=", k)
 }
