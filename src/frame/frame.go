e95cf606a21870c03c5b4799acb1186ef0870738
hill.gao <hill.gao@yuansuan.com>
2023-12-14 18:36:41 +0800

    读取hello.html的文件，作为响应数据

--- src/frame/frame.go
+++ src/frame/frame.go
@@ -2,12 +2,24 @@ package frame
 
 import (
 	"fmt"
+	"io/ioutil"
 	"main/frame/gin"
 	"net/http"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello HTTP!")
+	// fmt.Fprintln(w, "<h1>Hello HTTP!</h1>")
+	b, err := ioutil.ReadFile("hello.html")
+	if err != nil {
+		fmt.Println("ReadFile failed, err=", err)
+		return
+	}
+	n, err := fmt.Fprintln(w, string(b))
+	if err != nil {
+		fmt.Println("Fprintln failed, err=", err)
+		return
+	}
+	fmt.Println("Fprintln len=", n)
 }
 
 func httpDemo() {
